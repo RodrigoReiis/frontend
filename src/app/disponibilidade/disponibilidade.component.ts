6967df1672f6fd19f57fe361cc52dc8aca3f60d6
Rodrigo Reis <roo_reiis@hotmail.com>
2024-10-04 13:23:25 -0300

    feat: Ajustes visibilidade e campos de disponibilidade

--- src/app/disponibilidade/disponibilidade.component.ts
+++ src/app/disponibilidade/disponibilidade.component.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-disponibilidade',
   templateUrl: './disponibilidade.component.html',
-  styleUrls: ['./disponibilidade.component.scss']
+  styleUrls: ['./disponibilidade.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class DisponibilidadeComponent {
+  isDiaTodo = true;
 
+  form = new FormGroup({
+    data: new FormControl(),
+    diaTodo: new FormControl('Sim'),
+  })
+
+  changeDiaTodo(value: boolean) {
+    this.isDiaTodo = value;
+  }
 }
