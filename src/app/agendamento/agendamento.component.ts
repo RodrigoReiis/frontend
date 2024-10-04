29bf54ac00126c2fe846bb7181b19cd34add3d29
Rodrigo Reis <roo_reiis@hotmail.com>
2024-10-04 12:34:15 -0300

    feat: Adicionado componente de agendamento como dialog da listagem de medicos

--- src/app/agendamento/agendamento.component.ts
+++ src/app/agendamento/agendamento.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Medicos } from 'src/models/Response/medicos.model';
+import { MatFormFieldModule, MatHint } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input'
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+
+
+
 
 @Component({
   selector: 'app-agendamento',
   templateUrl: './agendamento.component.html',
-  styleUrls: ['./agendamento.component.scss']
+  standalone: true,
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+    provideMomentDateAdapter(),
+  ],
+  imports: [ReactiveFormsModule,MatInputModule, MatFormFieldModule, MatSelectModule, MatDatepickerModule, MatHint],
+  styleUrls: ['./agendamento.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AgendamentoComponent {
+  form = new FormGroup({
+    idMedico: new FormControl(),
+    dataAgendamento: new FormControl(),
+  })
 
+  formData = new FormGroup({
+    dataInicio: new FormControl(),
+    dataFim: new FormControl()
+  })
+
+  private readonly _locale = signal(inject<unknown>(MAT_DATE_LOCALE));
+  readonly dateFormatString = computed(() => {
+    if (this._locale() === 'pt-BR') {
+      return 'DD/MM/YYYY';
+    }
+    return '';
+  });
+
+  readonly dialogRef = inject(MatDialogRef<AgendamentoComponent>);
+  dialogData = inject(MAT_DIALOG_DATA);
+  idMedico: string = this.dialogData.idMedico;
+  listaMedicos: Medicos[] = this.dialogData.listaMedicos;
+
+
+  realizarAgendamento() {
+    this.closeAgendamento();
+  }
+
+  closeAgendamento() {
+    this.dialogRef.close();
+  }
 }
--- src/app/medicos/medicos.component.ts
+++ src/app/medicos/medicos.component.ts
@@ -4,19 +4,23 @@ import { Medicos } from 'src/models/Response/medicos.model';
 import { CommonModule } from '@angular/common';
 import { catchError } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { AgendamentoComponent } from '../agendamento/agendamento.component';
 
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   providers: [MedicosService],
   styleUrls: ['./medicos.component.scss']
 })
+
+
 export class MedicosComponent implements OnInit {
   listaMedicos: Medicos[] = new Array<Medicos>();
 
-  constructor(private medicosService: MedicosService, private snackBar: MatSnackBar) {}
+  constructor(private medicosService: MedicosService, private snackBar: MatSnackBar, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.medicosService.getMedicos().subscribe({
@@ -28,5 +32,19 @@ export class MedicosComponent implements OnInit {
         this.snackBar.open(error.error, 'Fechar', { duration: 5000 });
       },
     });
+  }
+
+  agendarConsultaDialog(idMedico: string) {
+    this.dialog.open(AgendamentoComponent, {
+      width: '500px',
+      data: {
+        idMedico: idMedico,
+        listaMedicos: this.listaMedicos
+      }
+    })
+
+    this.dialog.afterAllClosed.subscribe((result) => {
+      console.log(result);
+    })
   }
 }
